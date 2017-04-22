088d00a718cb693657fe0f7137faf6ad9b73f892
unknown <tjdgns8047@naver.com>
2017-04-22 15:50:48 +0900

    Connect mongodb

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('Connected MongoDB');
+});
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
