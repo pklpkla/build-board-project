eb757ea0744775cf2854b1b3ed34da5868a69aaf
unknown <tjdgns8047@naver.com>
2017-04-22 15:23:21 +0900

    start ejs

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 app.set('view engine', 'ejs');
-app.set('index', './views');
+app.set('views', './views');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 app.listen(3000,function(){
   console.log('Connected port 3000!!!');
